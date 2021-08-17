e2e17fd3ce6c09835b0681475ffe064b799ee994
Theodore Kelechukwu Onyejiaku <theodoreonyejiaku@gmail.com>
2021-08-17 14:43:39 +0100

    initial commit

---
+++ app.js
@@ -0,0 +1,42 @@
+const express = require('express');
+const logger = require('morgan');
+const cors = require('cors');
+const path = require("path");
+require('dotenv').config();
+
+var app = express();
+
+app.use(cors());
+app.use(logger('dev'));
+app.use(express.json({limit: "50mb"}));
+app.use(express.urlencoded({limit:"50mb", extended: true}));
+app.use(express.static(path.join(__dirname, 'public')));
+
+//Mongodb connection
+const mongoose = require('mongoose');
+
+mongoose.connect(process.env.DB_LOCAL, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  useFindAndModify: false,
+  useCreateIndex: true
+});
+
+const db = mongoose.connection;
+
+db.on("error", ()=>{
+  console.error.bind(console, 'MongoDB connection error:')
+})
+
+db.on("open", ()=>{
+  console.log("database running successfully");
+})
+
+const indexRouter = require("./routes/index")
+
+app.use("/", indexRouter);
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, ()=>{
+  console.log("App running on port: "+PORT);
+})
