e69ef09330bfc2e59a214f2b8812eb9eb1a6c149
Theodore Kelechukwu Onyejiaku <theodoreonyejiaku@gmail.com>
2021-08-17 15:46:27 +0100

    set up routes and controllers

--- controllers/indexController.js
+++ controllers/indexController.js
@@ -23,13 +23,48 @@ exports.contactMe = async(req, res, next)=>{
         const protocol = req.protocol;
         const host = req.get("host");
         const link = `${protocol}://${host}/contact/${newContact._id}`;
-        const emailContent = `Hi ${name}, you have just contacted Theodre. And here is the link to your response`;
+        const emailContent = `Hi ${req.body.name}, you have just contacted Theodore. Below is the link to your response \n`;
     
+        const msg = {
+            to: newContact.email,
+            from: "theodore.onyejiaku.g20@gmail.com", // Use the email address or domain you verified above
+            subject: "Resume Response",
+            text: "Resume Response",
+            html: `<body>
+                      <h1  style='font-family:Tangerine, cursive'>Message from Theodore 🖐😎</h1>
+                          <p>${emailContent}</p>
+                          <p></p>
+                          <button style="background-color:#ee6e73;border:none;outline:none;color:white;padding:2%;cursor:pointer"><a href="${link}">Verify Account</a></button>
+                      <footer></footer>
+                  </body>
+                  `
+          };
+          sgMail.send(msg).then(
+            async (resp) => {
+              console.log("Sent Successfully")
+              console.log(link);
+              return res.status(200).json({status: "ok", message: "Sent successfully!", contact: newContact});
+            },
+            (error) => {
+              console.error(error.message);
+              return res.status(500).json({status: "fail", message:"Something went wrong"})
+            }
+          );
     }
     catch(error){
-
+        res.status(500).json({status: "fail", message: error.message})
     }
-    
+}
 
-    
+exports.getContactResponse = async(req, res, next)=>{
+    const contactId = req.params.contactId;
+    try {
+        const contact = await Contact.findById(contactId);
+        if(!contact){
+            res.status(404).json({status: "fail", message:"Could not find response"});
+        }
+        res.status(200).json({status: "ok", message: "response found", contact})
+    } catch (error) {
+        res.status(500).json({status: "fail", message: error.message})
+    }
 }
--- models/Contact.js
+++ models/Contact.js
@@ -5,7 +5,6 @@ const ContactSchema = new Schema({
     name: {type: String},
     email: {type: String},
     message: {type: String},
-    responseUrl : {type: String}
 })
 
 module.exports = mongoose.model("Contact", ContactSchema);
